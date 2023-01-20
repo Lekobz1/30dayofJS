9f187ebe38d3bd4569cde5718c583d6dd79f9811
lekobz1 <jonathan.simon.pro1@gmail.com>
2023-01-20 14:30:01 +0100

    jour1

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+console.log(typeof 'prenom');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+let bisou
+let maman
+let papa
+let chaton
+
+let chat = 'rosi';
+let cheval = 'hatgalyn';
+let fourPatte = true;
+let age2 = 5;
+
+let prenom = 'jonathan';
+let nom3 = "Simon";
+let etatCivile = "homme";
+let pays = "france";
+let age4 = `30`;
+
+let pre = 'solene' , n = 'dos santos' , civil = "femme" , city1 = `france`, a = 27;
+
+let myAge = 30;
+let yourAge = 27;
+console.log('i am ', myAge , ' years old');
+
+
---
+++ variable.js
@@ -0,0 +1,6 @@
+let nom = "Jonathan";
+let aFaim = true;
+let boire
+let fumer = null;
+
+console.log(nom, aFaim, boire, fumer);
